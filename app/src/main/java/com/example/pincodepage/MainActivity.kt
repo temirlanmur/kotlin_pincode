059079a43bc260390f11a8034d13b461cbc45f08
temirm <76197653+temirm@users.noreply.github.com>
2023-11-27 13:42:33 +0600

    Open account activity on successful pin

--- app/src/main/java/com/example/pincodepage/MainActivity.kt
+++ app/src/main/java/com/example/pincodepage/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.pincodepage
 
+import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
 import android.widget.TextView
@@ -82,6 +83,8 @@ class MainActivity : AppCompatActivity() {
                     this,
                     resources.getString(R.string.success_toast),
                     Toast.LENGTH_SHORT).show()
+                val intent = Intent(this, AccountActivity::class.java)
+                startActivity(intent)
             } else {
                 pinColor = getColor(R.color.text_error)
             }

f88845272a330b002421ea83a0801a5bea5f2e37
temirm <76197653+temirm@users.noreply.github.com>
2023-11-27 14:34:55 +0600

    Add implicit intents

--- app/src/main/java/com/example/pincodepage/AccountActivity.kt
+++ app/src/main/java/com/example/pincodepage/AccountActivity.kt
@@ -1,11 +1,60 @@
 package com.example.pincodepage
 
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
+import android.provider.MediaStore
+import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
 
 class AccountActivity : AppCompatActivity() {
+    private lateinit var btnShare: Button
+    private lateinit var btnMail: Button
+    private lateinit var btnCall: Button
+    private lateinit var btnCamera: Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_account)
+
+        initViews()
+
+        setClickListeners()
+    }
+
+    private fun initViews() {
+        btnShare = findViewById(R.id.btn_share)
+        btnMail = findViewById(R.id.btn_mail)
+        btnCall = findViewById(R.id.btn_call)
+        btnCamera = findViewById(R.id.btn_camera)
+    }
+
+    private fun setClickListeners() {
+        btnShare.setOnClickListener {
+            val sendIntent = Intent(Intent.ACTION_SEND).apply {
+                putExtra(Intent.EXTRA_TEXT, "text to share")
+                type = "text/plain"
+            }
+            val shareIntent = Intent.createChooser(sendIntent, null)
+            startActivity(shareIntent)
+        }
+
+        btnMail.setOnClickListener {
+            val intent = Intent(Intent.ACTION_SENDTO).apply {
+                data = Uri.parse("mailto:")
+            }
+            startActivity(intent)
+        }
+
+        btnCall.setOnClickListener {
+            val number = Uri.parse("tel:$SAMPLE_PHONE_NUMBER")
+            val intent = Intent(Intent.ACTION_DIAL, number)
+            startActivity(intent)
+        }
+
+        btnCamera.setOnClickListener {
+            val intent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+            startActivity(intent)
+        }
     }
 }
--- app/src/main/java/com/example/pincodepage/Constants.kt
+++ app/src/main/java/com/example/pincodepage/Constants.kt
@@ -1,3 +1,4 @@
 package com.example.pincodepage
 
 const val PASSWORD = "1567"
+const val SAMPLE_PHONE_NUMBER = "12345678"
